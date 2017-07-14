cc400c658d1c27311c82ceedbc38417e7fc5fa42
Dany <dany.bouca-nova@epitech.eu>
2017-07-14 16:57:42 +0200

    Set 1 - Challenge 3

--- Basics/challenge_1.py
+++ Basics/challenge_1.py
@@ -4,8 +4,8 @@ import codecs
 
 
 def hex_2_b64(s):
+    """Challenge 1 - Hexadecimal into base64 encoding"""
     return codecs.encode(codecs.decode(s, 'hex'), 'base64').decode()
 
 
-if __name__ == '__main__':
-   print(hex_2_b64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")),
+print(hex_2_b64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")),
--- Basics/challenge_2.py
+++ Basics/challenge_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-if __name__ == '__main__':
-    s1 = 0x1c0111001f010100061a024b53535009181c
-    s2 = 0x686974207468652062756c6c277320657965
 
-    print(hex(s1 ^ s2))
+def hex_xor(s1, s2):
+    """Challenge 2 - Hexadecimal xored"""
+    return hex(s1 ^ s2)
+
+
+print(hex_xor(0x1c0111001f010100061a024b53535009181c, 0x686974207468652062756c6c277320657965))
