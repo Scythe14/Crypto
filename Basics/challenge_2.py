79dd7b2567388e070c085829d8e53107251f8400
Dany Bouca Nova <dany.boucanova@gmail.com>
2020-01-02 11:30:20 +0100

    [Clean]	- Clean that old repository, removing first pyton scripts and directories

--- Basics/challenge_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-def hex_xor(s1, s2):
-    """Challenge 2 - Hexadecimal xored"""
-    return hex(s1 ^ s2)
-
-
-print(hex_xor(0x1c0111001f010100061a024b53535009181c, 0x686974207468652062756c6c277320657965))
